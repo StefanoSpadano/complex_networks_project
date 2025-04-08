ba7cad70d41a5d97586705a993e426e4d73a00ae
Stefano <stefano.spadano426@gmail.com>
2025-04-08 17:05:27 +0200

    Add utils.py inside which is stored a save_plot function to store the plots inside folders

---
+++ utils.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  8 15:57:19 2025
+
+@author: Raffaele
+"""
+
+import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+import inspect
+
+def save_plot(name_hint: str = "", folder: str = ""):
+    """
+    Saves the current matplotlib plot with an automatic filename based on timestamp and script name.
+
+    Parameters:
+    - name_hint (str): Optional description of the plot (e.g., 'degree_distribution')
+    - folder (str): Folder to save plots into.
+    """
+    # Create folder if it doesn't exist
+    os.makedirs(folder, exist_ok=True)
+
+    # Get script name (caller)
+    frame = inspect.stack()[1]
+    script_name = os.path.splitext(os.path.basename(frame.filename))[0]
+
+    # Timestamp
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # Compose file name
+    name_parts = [script_name]
+    if name_hint:
+        name_parts.append(name_hint)
+    name_parts.append(timestamp)
+    file_name = "_".join(name_parts) + ".png"
+
+    # Full path
+    full_path = os.path.join(folder, file_name)
+
+    # Save
+    plt.savefig(full_path, bbox_inches="tight")
+    print(f"✅ Plot saved as {full_path}")

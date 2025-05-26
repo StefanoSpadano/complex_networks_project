ceb2f0b6c5a46206cb7619a0a4227b728d59cebc
Stefano <stefano.spadano426@gmail.com>
2025-05-26 23:35:04 +0200

    Add compute_flow_values test and refactor compute_flow_value into utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,9 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from utils import categorize_sentiment
+from utils import categorize_sentiment, compute_flow_values
+import numpy as np
+
 
 
 def test_categorize_sentiment():
@@ -20,3 +22,19 @@ def test_categorize_sentiment():
     
     # Test positive score
     assert categorize_sentiment(0.5) == 'Positive'
+
+
+def test_compute_flow_values():
+    matrix = np.array([
+        [5, 2, 0],
+        [1, 3, 4],
+        [0, 0, 2]
+    ])
+
+    inter_flows, intra_flows = compute_flow_values(matrix)
+
+    assert (0, 0, 5) not in inter_flows
+    assert (1, 2, 4) in inter_flows
+    assert (0, 5) in intra_flows
+    assert len(inter_flows) == 3
+    assert len(intra_flows) == 3

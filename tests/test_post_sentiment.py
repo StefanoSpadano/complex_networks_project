7fc6203c6c01c0c02d17dbe28406a518075f44ca
Stefano <stefano.spadano426@gmail.com>
2025-06-16 11:23:28 +0200

    Add other test functions to test_post_sentiment.py

--- tests/test_post_sentiment.py
+++ tests/test_post_sentiment.py
@@ -6,7 +6,7 @@ Created on Thu Jun 12 18:53:45 2025
 """
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from analize_sentiment import calculate_sentiment, add_sentiment_to_posts
+from analize_sentiment import calculate_sentiment, add_sentiment_to_posts, clean_sentiment_data
 import pandas as pd
 
 
@@ -81,3 +81,66 @@ def test_add_sentiment_to_posts_basic():
     #at least 3 are strings entry
     assert result["sentiment_selftext"].notnull().sum() >= 3
 
+
+def test_add_sentiment_to_posts_with_empty_df():
+    """
+    Given an empty dataframe which only contains a column for the selftexts but no actual selftexts,
+    when the function add_sentiments_to_posts is called,
+    then an empty dataframe should be returned.
+    """
+    #Initialize an empty dataframe with a column called selftext
+    df = pd.DataFrame(columns=["selftext"])
+    #Initialize the sentiment analyzer
+    analyzer = SentimentIntensityAnalyzer()
+    #Initialize the result variable
+    result = add_sentiment_to_posts(df, analyzer)
+    #assert
+    assert result.empty
+
+def test_add_sentiments_to_posts_with_NaN_selftexts():
+    """
+    Given a dataframe containg only NaN values under the "selftext" column,
+    when the add_sentiment_to_posts function is called,
+    then the sum of all sentiment scores should be zero.
+    """
+    #Initialize a dataframe which contains only NaN values under the column "selftext"
+    df = pd.DataFrame({"selftext":[None, None, None]
+    })
+    #Initialize the sentiment analyzer
+    analyzer = SentimentIntensityAnalyzer()
+    #Initialize the result variable
+    result = add_sentiment_to_posts(df, analyzer)
+    #assert
+    assert result["sentiment_selftext"].sum() == 0.0
+
+
+def test_clean_sentiment_data_with_nans():
+    """
+    Given a dataframe containing selftexts and one of them is a NaN value,
+    when the clean_sentiment_data function is called,
+    then the resulting dataframe should not include the corresponding line for that missing value.
+    """
+    #Initialize a dataframe with a selftext column containing a NaN value,
+    #plus a column containing the corresponding sentiment scores
+    df = pd.DataFrame({
+        "selftext": ["Text 1", None, "Text 2"],
+        "sentiment_selftext": [0.1, 0.0, -0.2]
+    })
+    #Call the function to clean the data
+    cleaned = clean_sentiment_data(df)
+    #asserts
+    assert len(cleaned) == 2
+    assert cleaned.isnull().sum().sum() == 0
+
+def test_clean_sentiment_data_all_nans():
+    """
+    Given a dataframe containing all nans in the selftext column,
+    when the clean_sentiment_data function is called,
+    then should return an empty dataframe.
+    """
+    #Initialize a dataframe with all nan values in the selftext column
+    df = pd.DataFrame({"selftext":[None, None, None]})
+    #Call the function clean_sentiment_data
+    cleaned = clean_sentiment_data(df)
+    #assert
+    assert cleaned.empty
